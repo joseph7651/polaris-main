05a6ae1ed0714da35cfc2ff51416fb109bcfdfe2
joseph7651 <josephcastellano715@gmail.com>
2022-07-14 00:00:00 +0100

    chore: update wsgi

--- theme/apps.py
+++ theme/apps.py
@@ -7,3 +7,4 @@ class ThemeConfig(AppConfig):
 # touched on 2025-05-27T15:28:53.860936Z
 # touched on 2025-08-14T21:16:53.868769Z
 # touched on 2025-08-14T21:17:07.060348Z
+# touched on 2025-08-14T21:17:18.007982Z
